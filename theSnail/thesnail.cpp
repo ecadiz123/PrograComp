261ee0eb69222163ef02243f61a7141963002938
Esteban <estebancadiz@gmail.com>
2025-03-17 19:57:47 +0000

    init commmit

---
+++ theSnail/thesnail.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main ()
+{
+
+#ifndef VJUDGE	
+    freopen("input.txt","r",stdin);
+    freopen("output.txt","w",stdout);
+#endif
+
+
+    return 0;   
+
+}
